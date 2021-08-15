877e5eb5043fcb25f5bcaf29044f58f07773872d
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-08-15 12:37:58 +0530

    added deposit withdraw functionality

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useSelector,useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { actionCreators } from './state/index'
 
 function App() {
+  
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch()
+
+  const { depositMoney,withdrawMoney } = bindActionCreators(actionCreators, dispatch);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='App'>
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit </button>
+      <button onClick={() => withdrawMoney(500)}>Withdraw </button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/state/store.js
@@ -0,0 +1,9 @@
+import { createStore,applyMiddleware } from "redux";
+import reducers  from "./Reducers/index";
+import thunk from 'redux-thunk'
+
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(thunk)
+);
